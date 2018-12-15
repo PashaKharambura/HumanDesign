12d019a423a32102f2d237caeb78800d45689bdb
PashaKharambura <pasha.harambura@gmail.com>
2018-12-15 19:04:25 +0200

    Initial Commit

---
+++ HumanDesign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HumanDesign
+//
+//  Created by Pavlo Kharambura on 12/15/18.
+//  Copyright © 2018 Pavlo Kharambura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
