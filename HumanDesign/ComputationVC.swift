74e307ee38f1691ad3f887410dd0f3040fe263a1
PashaKharambura <pasha.harambura@gmail.com>
2018-12-16 22:18:52 +0200

    UserInfoVC

--- HumanDesign/ComputationVC.swift
+++ HumanDesign/ComputationVC.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ComputationVC: UIViewController {
 
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var month: UILabel!
+    @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var view3: UIView!
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var view4: UIView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var view5: UIView!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var view6: UIView!
+    
+    @IBOutlet weak var checkboxImage: UIImageView!
+    
+    @IBOutlet weak var countButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        initialViewConfigurations()
     }
 
-
+    private func initialViewConfigurations() {
+        configureBgViews()
+    }
+    
+    private func configureBgViews() {
+        let views = [view1,view2,view3,view4,view5,view6,countButton]
+        for view in views {
+            view?.layer.cornerRadius = 25
+        }
+    }
+    
+    @IBAction func ButtonAction(_ sender: Any) {
+        
+    }
+    
 }
 
