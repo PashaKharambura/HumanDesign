bb6d913247ab456bc5532823bda3ceb0daf1ffa8
PashaKharambura <pasha.harambura@gmail.com>
2018-12-16 20:47:30 +0200

    PersonalMapVC

--- HumanDesign/PersonalMapVC.swift
+++ HumanDesign/PersonalMapVC.swift
@@ -9,23 +9,74 @@
 
 import UIKit
 
+
+
 class PersonalMapVC: UIViewController {
 
+    private enum ViewState {
+        case getReport
+        case vaitReport
+    }
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var chartsImageView: UIImageView!
+    @IBOutlet weak var getReportButton: UIButton!
+    @IBOutlet weak var tfBgView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var okImageView: UIImageView!
+    @IBOutlet weak var secondTitle: UILabel!
+    @IBOutlet weak var secondSubtitle: UILabel!
+    
+    
+    private var viewState: ViewState = .getReport
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setInitialViewConfigurations()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setInitialViewConfigurations() {
+        tfBgView.layer.cornerRadius = 25
+        getReportButton.layer.cornerRadius = 25
+        setViewElementsActive(state: .getReport)
     }
-    */
+    
+    private func setViewElementsActive(state: ViewState) {
+        switch state {
+        case .getReport:
+            titleLabel.isHidden = false
+            subtitleLabel.isHidden = false
+            chartsImageView.isHidden = false
+            tfBgView.isHidden = false
+            secondTitle.isHidden = true
+            secondSubtitle.isHidden = true
+            okImageView.isHidden = true
+            getReportButton.setTitle("Заказать расчет", for: .normal)
+            
+            return
+        case .vaitReport:
+            titleLabel.isHidden = true
+            subtitleLabel.isHidden = true
+            chartsImageView.isHidden = true
+            tfBgView.isHidden = true
+            secondTitle.isHidden = false
+            secondSubtitle.isHidden = false
+            okImageView.isHidden = false
+            getReportButton.setTitle("Рассчитать бодиграф", for: .normal)
+            
+            return
+        }
+    }
 
+    @IBAction func buttonAtion(_ sender: Any) {
+//        if viewState == .getReport {
+//            viewState = .vaitReport
+//        } else {
+//            viewState = .getReport
+//        }
+//        setViewElementsActive(state: viewState)
+    }
 }
